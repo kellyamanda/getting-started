771ba9a4354e7e35d7bcd0f00ee3539464d3b6bc
Amanda Kelly <47222480+kellyamanda@users.noreply.github.com>
2021-03-26 14:07:51 -0700

    Update get_started.py

--- get_started.py
+++ get_started.py
@@ -4,4 +4,19 @@ import pandas as pd
 import altair as alt
 
 st.set_page_config(page_title="Streamlit Getting Started Guide", initial_sidebar_state="expanded")
-st.title("test")
+
+state = st.get_state()
+
+options = ("🏃‍♀️Getting Started",
+    "⬇️ Installing Streamlit",
+    "🏗 Basic Functions",
+    "🎨 Layout and Themes",
+    "🏎 App Performance",
+    "🚀 Deploying your App",
+    "🎈 More Resources")
+
+nav = st.sidebar.selectbox("Choose a section", options, key="nav")
+
+def on_next_click():
+    state.nav = options[options.index(state.nav) + 1]
+
